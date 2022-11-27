277b0ea79b96c5f52072484ba947e3b389bc13fd
TonyMckes <tonymckes@gmail.com>
2022-11-27 14:35:28 -0400

    feat: add model seeds

---
+++ database/seeders/20221127164859-create-users.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = Array(5)
+      .fill(null)
+      .map((_, index) => ({
+        username: `exampleUser${index + 1}`,
+        email: `example${index + 1}@mail.com`,
+        password: `examplePwd${index + 1}`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }));
+
+    await queryInterface.bulkInsert("Users", users, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
