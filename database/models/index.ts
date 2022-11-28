e34dcf55c47c68e8a7360830c811b5411158fd13
TonyMckes <tonymckes@gmail.com>
2022-11-28 17:30:27 -0400

    refactor: prepare exports for typescript

---
+++ database/models/index.ts
@@ -0,0 +1,36 @@
+import { env as _env } from "process";
+import { DataTypes, Sequelize } from "sequelize";
+import ArticleModel from "./article";
+import CommentModel from "./comment";
+import TagModel from "./tag";
+import UserModel from "./user";
+const env = _env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+
+let sequelize;
+if (config.use_env_variable) {
+  // @ts-ignore
+  sequelize = new Sequelize(_env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+const Article = ArticleModel(sequelize, DataTypes);
+const Comment = CommentModel(sequelize, DataTypes);
+const Tag = TagModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
+
+const db: { [key: string]: any } = { Article, Comment, Tag, User };
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+export { Article, Comment, Tag, User, sequelize, Sequelize };
---
+++ database/models/model-types.d.ts
@@ -0,0 +1,7 @@
+import { Article, Comment, sequelize, Tag, User } from ".";
+
+type Article = typeof Article;
+type Tag = typeof Tag;
+type Comment = typeof Comment;
+type User = typeof User;
+type ConnectionInstance = typeof sequelize;
