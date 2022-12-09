0e45e6e475f5271244290f0a564c52e1d1c48260
TonyMckes <tonymckes@gmail.com>
2022-12-09 11:48:02 -0400

    feat: add tests for register user service

---
+++ services/registerUser.test.ts
@@ -0,0 +1,47 @@
+import { User } from "database/models";
+import registerUser from "./registerUser";
+
+describe("service: register new user", () => {
+  const newUserMock = {
+    email: "test@mail.com",
+    password: "123456",
+    username: "testUser",
+  };
+  const createdUserMock = {
+    user: {
+      ...newUserMock,
+      bio: null,
+      createdAt: undefined,
+      following: false,
+      id: undefined,
+      image: null,
+      password: undefined,
+      updatedAt: undefined,
+    },
+  };
+
+  const findUserAndDelete = async ({ username }: typeof newUserMock) => {
+    const userInDb = await User.findOne({
+      where: { username },
+    });
+
+    if (!userInDb) return;
+    await userInDb.destroy();
+  };
+
+  beforeAll(async () => await findUserAndDelete(newUserMock));
+  afterAll(async () => await findUserAndDelete(newUserMock));
+
+  it("should be a function", () => {
+    expect(typeof registerUser).toBe("function");
+  });
+
+  it("should return a matching object", async () => {
+    const user = await registerUser(newUserMock);
+    expect(user).toMatchObject(createdUserMock);
+  });
+
+  it("should to throw if the email already exists", async () => {
+    await expect(registerUser(newUserMock)).rejects.toThrow();
+  });
+});
