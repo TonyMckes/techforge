5c55bd190d1f46bcca8f3a458dd2937a92d36222
TonyMckes <tonymckes@gmail.com>
2022-12-13 17:38:06 -0400

    feat: add user login endpoint

---
+++ pages/api/users/login.ts
@@ -0,0 +1,28 @@
+import { apiRouteWithSession } from "lib/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import userLogin from "services/userLogin";
+
+export default apiRouteWithSession(usersRoute);
+
+async function usersRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { method, session } = req;
+  const { user: userData } = req.body;
+
+  try {
+    if (method === "POST") {
+      const user = await userLogin(userData);
+
+      session.user = user;
+      session.isLoggedIn = true;
+      await session.save();
+
+      return res.json({ user });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ errors: { body: [error.message] } });
+    }
+  }
+
+  res.status(405).json("Method Not Allowed");
+}
