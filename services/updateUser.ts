2148f9daa87c570eb70ad97b00692a0733a8a814
TonyMckes <tonymckes@gmail.com>
2022-12-14 11:57:00 -0400

    feat: add update user service

---
+++ services/updateUser.ts
@@ -0,0 +1,30 @@
+import { hash } from "bcrypt";
+import { User } from "database/models";
+import { CreationAttributes } from "sequelize";
+
+async function updateUser(
+  userData: CreationAttributes<User>,
+  loggedUser: User
+) {
+  const { password }: { password: string } = userData;
+  const userInDb = await User.findOne({ where: { email: loggedUser.email } });
+  if (!userInDb) throw new Error("UnauthorizedError");
+
+  Object.entries(userData).forEach((entry) => {
+    const [key, value] = entry;
+
+    if (value !== undefined && key !== "password") {
+      userInDb.set(key, value);
+    }
+  });
+
+  if (password && password.length > 5) {
+    userInDb.set("password", await hash(password, 10));
+  }
+
+  await userInDb.save();
+
+  return userInDb;
+}
+
+export default updateUser;
