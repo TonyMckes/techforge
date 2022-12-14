1362f110cbb72377a2c55326aca0285b048a1aff
TonyMckes <tonymckes@gmail.com>
2022-12-14 13:41:58 -0400

    feat: add endpoint to update user

---
+++ pages/api/user.ts
@@ -0,0 +1,28 @@
+import { apiRouteWithSession } from "lib/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import updateUser from "services/updateUser";
+
+export default apiRouteWithSession(usersRoute);
+
+async function usersRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { method, session } = req;
+  const { user: userData } = req.body;
+
+  try {
+    if (method === "PUT") {
+      if (!session.user) throw new Error("You need to login first!");
+      const user = await updateUser(userData, session.user);
+
+      session.user = user;
+      await session.save();
+
+      return res.status(200).json({ user });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ errors: { body: [error.message] } });
+    }
+  }
+
+  res.status(405).json("Method Not Allowed");
+}
