f518d98b0f8ad9777b1c15b80632cecf938eb52f
TonyMckes <tonymckes@gmail.com>
2022-12-19 11:21:35 -0400

    feat: add get article by slug endpoint

---
+++ pages/api/articles/[slug]/index.ts
@@ -0,0 +1,26 @@
+import { apiRouteWithSession } from "lib/session";
+import type { NextApiRequest, NextApiResponse } from "next";
+import deleteArticle from "services/articles/deleteArticle";
+import getArticle from "services/articles/getArticle";
+
+export default apiRouteWithSession(usersRoute);
+
+async function usersRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const { slug } = req.query;
+  const { user } = req.session;
+
+  try {
+    if (method === "GET") {
+      const article = await getArticle(slug as string, user);
+
+      return res.json({ article });
+    }
+
+    res.status(405).json("Method Not Allowed");
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ errors: { body: [error.message] } });
+    }
+  }
+}
