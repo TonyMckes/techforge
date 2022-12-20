574ce5b6ed108ce38fa667e5123aba5b7d871ea1
TonyMckes <tonymckes@gmail.com>
2022-12-20 12:19:44 -0400

    fix: require authentication

--- pages/api/profiles/[username]/index.ts
+++ pages/api/profiles/[username]/index.ts
@@ -1,4 +1,5 @@
 import { User } from "database/models";
+import { appendFollowers } from "lib/helpers";
 import { apiRouteWithSession } from "lib/session";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -7,10 +8,25 @@ export default apiRouteWithSession(usersRoute);
 async function usersRoute(req: NextApiRequest, res: NextApiResponse) {
   const { method, query } = req;
   const { username } = query;
+  const { user } = req.session;
 
   try {
     if (method === "GET") {
-      const profile = await User.findOne({ where: { username } });
+      if (!user) {
+        return res
+          .status(401)
+          .json({ errors: { body: ["UnauthorizedError"] } });
+      }
+      const loggedUser = await User.findOne({ where: { id: user.id } });
+      const profile = await User.findOne({
+        where: { username },
+        attributes: { exclude: ["email"] },
+      });
+      if (!profile) {
+        return res.status(403).json({ errors: { body: ["NotFound"] } });
+      }
+
+      await appendFollowers(profile, loggedUser);
 
       return res.json({ profile });
     }
