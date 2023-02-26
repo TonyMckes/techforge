2d1a13ab78144cf478d0c07b1264de1b76da33cc
Anthony Mackensen <tonymckes@gmail.com>
2023-02-26 14:11:40 -0400

    refactor: simplify and remove types

--- database/models/index.ts
+++ database/models/index.ts
@@ -1,40 +1,36 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { env as _env } from "process";
 import { Sequelize } from "sequelize";
+import sequelizeConfig from "../config/config";
 import ArticleModel, { Article } from "./article";
 import CommentModel, { Comment } from "./comment";
 import TagModel, { Tag } from "./tag";
 import UserModel, { User } from "./user";
 const env = _env.NODE_ENV || "development";
-const config = require("../config/config")[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  // @ts-ignore
-  sequelize = new Sequelize(_env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const config = sequelizeConfig[env];
+
+const sequelize = new Sequelize(
+  config.database!,
+  config.username!,
+  config.password!,
+  config
+);
 
 ArticleModel(sequelize);
 CommentModel(sequelize);
 TagModel(sequelize);
 UserModel(sequelize);
 
-const db: { [key: string]: any } = { Article, Comment, Tag, User };
+const db = { Article, Comment, Tag, User };
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+  if (db[modelName as keyof typeof db]["associate"]) {
+    db[modelName as keyof typeof db].associate(db);
   }
 });
 
-export { Article, Comment, Tag, User, sequelize, Sequelize };
-export type ConnectionInstance = typeof sequelize;
+export { Article, Comment, Tag, User, sequelize };
 export interface AssociatesTypes {
   Article: typeof Article;
   Comment: typeof Comment;
